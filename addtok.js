123b5670ae07bbad9b3bc1a1575768dbeb076f80
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-07 12:18:36 -0400

    Console logging result of sum and if else statement

--- addtok.js
+++ addtok.js
@@ -7,13 +7,16 @@ var numberList = [3, 15, 10, 7];
 var sum; 
                                                 //auto incrementer 
         for ( var z = 0; z < numberList.length; z++){
-            console.log("o:", numberList[z]);
+            // console.log("o:", numberList[z]);
             for ( var y = 0; y < numberList.length; y++){
-                    console.log(numberList[y]);
-                if( numberList[y] == numberList[y]) {
+                    // console.log(numberList[y]);
+                if( numberList[z] == numberList[y]) {
                     // do nothing
                 } else {
                     sum = numberList[z] + numberList[y];
+                    if(sum == k) {
+                        console.log(sum, true);
+                    }
                 }
                 
             }
